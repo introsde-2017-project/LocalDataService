0ddca922780808621c7fbd9644a52b6ee98f8a8b
Danish Cheema <danishcheemapk@yahoo.com>
2018-05-28 00:41:23 +0200

    make it better

--- src/introsde/project/data/local/model/FoodType.java
+++ src/introsde/project/data/local/model/FoodType.java
@@ -14,6 +14,7 @@ public enum FoodType implements Serializable {
 	Thai("Thai"),
 	Mexican("Mexican");
 	
+	
 	private String name;
 //	@ManyToOne
 //	@JoinColumn(name="personId",referencedColumnName="personId")
